81cc2520fdeec9846b11f658524b4f71db1d38eb
ram-9047 <ramg786@live.com>
2020-03-01 22:20:37 +0530

    sign-up and sing-in pages completed

---
+++ pages/signin.js
@@ -0,0 +1,68 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+export default function SignIn() {
+  return (
+    <Layout>
+      <div className=" h-full w-screen">
+        <h2 className="text-center text-3xl mt-20">Sign In</h2>
+        <div className="text-center cursor-pointer">
+          <span className="text-xs ml20  text-yellow-600">
+            <Link href="/signup"> Need an account</Link>
+          </span>
+        </div>
+
+        <form className="w-2/4 items-center  content-around ml-64 ">
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                for="inline-full-name"
+              >
+                Email
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                id="inline-full-name"
+                type="email"
+                placeholder="example@email.com"
+              />
+            </div>
+          </div>
+
+          <div className="md:flex md:items-center mb-6">
+            <div className="md:w-1/3">
+              <label
+                className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+                for="inline-username"
+              >
+                Password
+              </label>
+            </div>
+            <div className="md:w-2/3">
+              <input
+                className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+                id="inline-username"
+                type="password"
+                placeholder="******************"
+              />
+            </div>
+          </div>
+          <div className="md:flex md:items-center">
+            <div className="md:w-1/3"></div>
+            <div className="md:w-2/3">
+              <button
+                className="shadow bg-purple-500 hover:bg-purple-400 focus:outline-none text-white font-bold py-2 px-4 rounded"
+                type="button"
+              >
+                Sign In
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </Layout>
+  );
+}
