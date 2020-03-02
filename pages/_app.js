42b11f68bf208cc857e3cd6b7320637b1d708be8
ram-9047 <ramg786@live.com>
2020-03-02 21:30:41 +0530

    configure custom css in tailwind

---
+++ pages/_app.js
@@ -0,0 +1,11 @@
+import React from "react";
+import App from "next/app";
+
+import "../stylesheet/style.css";
+
+export default class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return <Component {...pageProps} />;
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        "custom-yellow": "#ffcf11",
+        "orange-550": "#F48023",
+        "orange-450": "#F2720C",
+        "pink-white-100": "#F7F6F9",
+        "blue-C": "#2B2C6E"
+      }
+    }
+  }
+};
